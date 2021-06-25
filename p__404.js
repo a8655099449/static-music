a4f6456fd38eb1b8a962f785b6f97f9f980a5ec3
kuangwei <865509949@qq.com>
2021-06-25 15:51:08 +0800

    2021年6月25日15:51:08

--- p__404.js
+++ p__404.js
@@ -1,1 +1,1 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[4],{pssB:function(n,e,s){"use strict";s.r(e);var t=s("q1tI"),a=s.n(t);e["default"]=()=>a.a.createElement("div",null,"404\u9875\u9762")}}]);
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[4],{pssB:function(n,e,t){"use strict";t.r(e);var s=t("q1tI"),u=t.n(s);e["default"]=function(){return u.a.createElement("div",null,"404\u9875\u9762")}}}]);
